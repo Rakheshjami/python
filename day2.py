9c907f523c3d48a1e7aa5f880ba7b50c29d8af0a
Rakesh <jamirakesh123@gmail.com>
2026-05-14 15:51:31 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,52 @@
+'''
+operators
+--------------
+Arithmetic operators'''
+a=2
+b=5
+print(b+a)
+print(b-a)
+print(a*b)
+print(b/a)
+
+'''Assignment operators'''
+c=a+b
+d=b-a
+print(c)
+print(d)
+
+'''comparision operators'''
+print(a>b)
+print(a<b)
+print(a==b)
+print(a<=b)
+print(a>=b)
+print(a!=b)
+
+'''identity operatios'''
+a1=5
+b1=5
+print(a1 is b1)
+print(a1 is not b1)
+
+'''membership operators'''
+arr=[1,2,3,4]
+print(b in arr)
+print(b not in arr)
+
+'''bitwise operators'''
+print(a&b)
+print(a|b)
+print(a^b)
+print(~a)
+
+'''strings'''
+str1="welcome all"
+print(str1.split('o'))
+print(len(str1))
+print(str1[::-1])
+print("y".join(str1))
+print(len(str1))
+print(str1[2:7])
+
+
