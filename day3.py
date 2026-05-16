e5c204004aed3df31ea264c28d1865285a554af2
Rakesh <jamirakesh123@gmail.com>
2026-05-16 08:39:20 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,25 @@
+'''
+List
+-------------
+'''
+list1=[1,"python",[1,3]]
+print(list)
+list2=[1,"python",[1,2,[34,"this is python 3rd class",78],"python is a language",89],34,[3,4]]
+print(list2[2][2][1][8])
+print(list2[2][4])
+arr=[1,2,3,4,5]
+arr.append(6)
+print(arr)
+arr.append([2,7])
+print(arr)
+arr.extend([1,2,3,4])
+arr.extend("python")
+print(arr)
+arr.pop(2)
+print(arr)
+arr.remove(3)
+print(arr)
+s="python"
+print(s.replace("y",""))
+print(s)
+
